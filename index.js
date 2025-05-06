3b0a96787996d2ef15bfaca3662667c08d617008
Alejandro Muñoz <alex12munozgeo@hotmail.es>
2025-05-06 17:08:09 +0200

    commit en nueva rama

--- index.js
+++ index.js
@@ -1,1 +1,3 @@
-console.log("Hola, este es el priemer archivo de la segunda actividad 🚀");
+console.log("Hola, este es el primer archivo de la segunda actividad 🚀");
+
+console.log("Este archivo se esta creando en la rama CambioLog");
