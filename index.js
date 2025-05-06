d943b071f612b9845dadd3eb4452fa4959e98782
Alejandro Muñoz <alex12munozgeo@hotmail.es>
2025-05-06 17:10:43 +0200

    cambio primer log

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
-console.log("Hola, este es el primer archivo de la segunda actividad 🚀");
+console.log(
+  "Hola, este es el primer archivo de la segunda actividad 🚀 Esta nueva linea de codigo, tambien está hecha desde la rama"
+);
 
 console.log("Este archivo se esta creando en la rama CambioLog");
